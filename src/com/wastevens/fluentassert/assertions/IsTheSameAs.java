9b3f191a8034697ec875d8153b9d1667e1d0f9bc
Drew Stevens <wastevens@gmail.com>
2011-10-16 16:10:18 -0400

    Refactored the IsTheSameAs assertion to allow for cleaner not-ing.

--- src/com/wastevens/fluentassert/assertions/IsTheSameAs.java
+++ src/com/wastevens/fluentassert/assertions/IsTheSameAs.java
@@ -1,22 +1,42 @@
 package com.wastevens.fluentassert.assertions;
 
+import com.wastevens.fluentassert.exception.ExpectedNotSameButWereSameException;
 import com.wastevens.fluentassert.exception.ExpectedSameButWereNotException;
 
 public class IsTheSameAs implements Assertion {
 
     private final Object expected;
+    
+    private Boolean notTheSameAs = false;
 
     private IsTheSameAs(Object expected) {
         this.expected = expected;
     }
-    
-    @Override
-    public void assertAgainst(Object actual) {
-        if (expected != actual)
-            throw new ExpectedSameButWereNotException(expected, actual);
+
+    public IsTheSameAs not() {
+        this.notTheSameAs = true;
+        return this;
     }
     
     public static IsTheSameAs isTheSameAs(Object expected) {
         return new IsTheSameAs(expected);
+    }
+    
+    @Override
+    public void assertAgainst(Object actual) {
+        if (!notTheSameAs)
+            assertExpectedIsTheSameAs(actual);
+        else
+            assertExpectedIsNotTheSameAs(actual);
+    }
+
+    private void assertExpectedIsTheSameAs(Object actual) {
+        if (expected != actual)
+            throw new ExpectedSameButWereNotException(expected, actual);
+    }
+    
+    private void assertExpectedIsNotTheSameAs(Object actual) {
+        if (expected == actual)
+            throw new ExpectedNotSameButWereSameException(expected, actual);
     }
 }
--- test/com/wastevens/fluentassert/assertions/IsTheSameAsTest.java
+++ test/com/wastevens/fluentassert/assertions/IsTheSameAsTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
+import com.wastevens.fluentassert.exception.ExpectedNotSameButWereSameException;
 import com.wastevens.fluentassert.exception.ExpectedSameButWereNotException;
 
 public class IsTheSameAsTest {
@@ -27,7 +28,7 @@ public class IsTheSameAsTest {
         Object expected = new Object();
         Object actual = new Object();
         
-        verifyProperExceptionThrown(expected, actual);
+        verifyProperExceptionThrownForExpectedSameButWereNot(expected, actual);
     }
        
     @Test
@@ -35,7 +36,7 @@ public class IsTheSameAsTest {
         Object expected = new Object();
         Object nullActual = null;
         
-        verifyProperExceptionThrown(expected, nullActual);
+        verifyProperExceptionThrownForExpectedSameButWereNot(expected, nullActual);
     }
     
     @Test
@@ -43,10 +44,10 @@ public class IsTheSameAsTest {
         Object nullExpected = null;
         Object actual = new Object();
         
-        verifyProperExceptionThrown(nullExpected, actual);
+        verifyProperExceptionThrownForExpectedSameButWereNot(nullExpected, actual);
     }
-    
-    private void verifyProperExceptionThrown(Object expected, Object actual) {
+ 
+    private void verifyProperExceptionThrownForExpectedSameButWereNot(Object expected, Object actual) {
         try {
             isTheSameAs(expected).assertAgainst(actual);
             fail("Expected exception");
@@ -55,4 +56,56 @@ public class IsTheSameAsTest {
             assertEquals(new ExpectedSameButWereNotException(expected, actual).getMessage(), exception.getMessage());
         }        
     }
+    
+    // not cases
+    
+    @Test
+    public void shouldThrowExceptionExpectedNotSameButWereSameOnTwoSameObjects() {
+        Object actual = new Object();
+        Object expected = actual;
+        
+        verifyProperExceptionThrownForExpectedNotSameButWereSame(expected, actual);
+    }
+    
+    @Test
+    public void shouldThrowExceptionExpectedNotSameButWereSameWhenGivenTwoNulls() {
+        Object actual = null;
+        Object expected = null;
+        
+        verifyProperExceptionThrownForExpectedNotSameButWereSame(expected, actual);
+    }
+    
+    @Test
+    public void shouldDoNothingnWhenGivenTwoDistinctObjects() {
+        Object actual = new Object();
+        Object expected = new Object();
+        
+        isTheSameAs(expected).not().assertAgainst(actual);
+    }
+    
+    @Test
+    public void shouldDoNothingWhenGivenNullForActualAndAnObjectWasExpected() {
+        Object nullActual = null;
+        Object expected = new Object();
+        
+        isTheSameAs(expected).not().assertAgainst(nullActual);
+    }
+    
+    @Test
+    public void shouldDoNothingWhenGivenNullForExpectedAndAnObjectWasActual() {
+        Object actual = new Object();
+        Object nullExpected = null;
+        
+        isTheSameAs(nullExpected).not().assertAgainst(actual);
+    }
+    
+    private void verifyProperExceptionThrownForExpectedNotSameButWereSame(Object expected, Object actual) {
+        try {
+            isTheSameAs(expected).not().assertAgainst(actual);
+            fail("Expected exception");
+        } catch(ExpectedNotSameButWereSameException exception) {
+            //TODO: Revisit this once FluentAssertion isValueEqual() is written
+            assertEquals(new ExpectedNotSameButWereSameException(expected, actual).getMessage(), exception.getMessage());
+        }        
+    }    
 }
